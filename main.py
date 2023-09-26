a4f10e4b9eca641860248ba1357b4b469b10647c
sp1rale <dimayaremchyk24@gmail.com>
2023-09-26 15:03:05 +0300

    EX 5 task was done

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-num1= float(input('Введіть перше число'))
-num2 = float(input('Введіть друге число'))
+num1 = float(input("Введіть перше число: "))
+num2 = float(input("Введіть друге число: "))
 
-min_num = min (num1,num2)
-print(f'мінімальне з {num1} і {num2} - це {min_num}')
+operation = input("Виберіть операцію (+ для суми, - для різниці, * для добутку, / для середнього арифметичного): ")
+
+if operation == '+':
+    result = num1 + num2
+elif operation == '-':
+    result = num1 - num2
+elif operation == '*':
+    result = num1 * num2
+elif operation == '/':
+    result = (num1 + num2) / 2
+else:
+    result = "Неправильна операція"
+
+print(f"Результат: {result}")
