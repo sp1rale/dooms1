253d4b801a8d23d5d238c4ccef4289e923e7f18c
sp1rale <dimayaremchyk24@gmail.com>
2023-09-26 14:40:51 +0300

    EX 4 task was done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 num1= float(input('Введіть перше число'))
 num2 = float(input('Введіть друге число'))
 
-max_num = max(num1,num2)
-print(f'максимум з {num1} і {num2} - це {max_num}')
+min_num = min (num1,num2)
+print(f'мінімальне з {num1} і {num2} - це {min_num}')
