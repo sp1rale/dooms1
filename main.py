bb92366f9d958cb5caa8660d2df6c57963141929
sp1rale <dimayaremchyk24@gmail.com>
2023-09-27 15:17:33 +0300

    EX 2  task was done

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ num1 = float(input('Введіть перше число'))
 num2 = float(input('Введіть друге число'))
 num3 = float(input('Введіть третє число'))
 
-operation = input('Виберіть операцію(+ для суми,* для добутку):')
-
-if operation == '+':
-    result = num1+num2+num3
-elif operation == '*':
-    result = num1*num2*num3
+operation = input("Виберіть операцію (max для максимуму, min для мінімуму, avg для середнього арифметичного): ")
+if operation == 'max':
+    result = max(num1,num2,num3)
+elif operation == 'min':
+    result = min(num1,num2,num3)
+elif operation == 'avg':
+    result = (num1,num2,num3)
 else:
-    result = 'Неправильна операція'
-
-print(f'Результат: {result}')
+    result=('Невірна операція')
+print(f'Результат:{result}:')
